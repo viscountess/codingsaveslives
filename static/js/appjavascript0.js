a004580ef1cac595d03395a0c678355ee40d52a3
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2020-04-21 17:58:59 +0100

    set up files for all pages
    looked at organising better inside flask but tbc how to do that effectively. set up 'welcome' notification re. emergency error page

---
+++ static/js/appjavascript0.js
@@ -0,0 +1,4 @@
+//explaining the 'm a' Error page shortcut
+var welcome = "Welcome to Maia! If you ever need to hide your screen, just click the button on the side, or type 'm a' and we will show an Error page!";
+alert(welcome);
+
