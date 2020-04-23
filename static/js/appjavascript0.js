ad79c4a497bea3ab8603fb00009d6127ab1da9a5
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2020-04-23 14:42:59 +0100

    key press trigger draft 1
    works with m key

--- static/js/appjavascript0.js
+++ static/js/appjavascript0.js
@@ -1,10 +1,11 @@
-//explaining the 'm a' Error page shortcut
-var welcome = "Welcome to Maia! If you ever need to hide your screen, just click the button on the side, or type 'm a' and we will show an Error page!";
-alert(welcome);
+//explaining the m Error page shortcut;
+var welcome = "Welcome to Maia! If you ever need to hide your screen, just click the button on the side, or type 'm' and we will show an Error page! For your safety, it will not be possible to see what you were previously looking at by then pressing the 'back' button.";
+    (alert(welcome));
 
-//m a trigger
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'm' && event.key === 'a') {
-      alert('Undo!');
+
+//m trigger;
+(document.addEventListener('keypress', function(event) {
+    if (event.key === 'm');{
+        window.location.replace("Error_404_Dispplay.html");
     }
-  });
+  }));
