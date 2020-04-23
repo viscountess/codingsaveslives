d20af1345099c84d34040663d3686d75cbb5523b
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2020-04-23 14:08:29 +0100

    button
    made and linked button in home page) for error display

--- static/js/appjavascript0.js
+++ static/js/appjavascript0.js
@@ -2,3 +2,9 @@
 var welcome = "Welcome to Maia! If you ever need to hide your screen, just click the button on the side, or type 'm a' and we will show an Error page!";
 alert(welcome);
 
+//m a trigger
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'm' && event.key === 'a') {
+      alert('Undo!');
+    }
+  });
