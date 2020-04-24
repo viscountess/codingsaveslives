9b0faefb34fff0dbba24b3691cdae05a6fdd69f3
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2020-04-24 16:22:31 +0100

    updated javascript

--- static/js/appjavascript0.js
+++ static/js/appjavascript0.js
@@ -1,4 +1,11 @@
-//explaining the 'm a' Error page shortcut
-var welcome = "Welcome to Maia! If you ever need to hide your screen, just click the button on the side, or type 'm a' and we will show an Error page!";
-alert(welcome);
+//explaining the m Error page shortcut;
+var welcome = "Welcome to Maia! If you ever need to hide your screen, just click the button on the side, or type 'm' and we will show an Error page! For your safety, it will not be possible to see what you were previously looking at by then pressing the 'back' button.";
+    (alert(welcome));
 
+
+//m trigger;
+(document.addEventListener('keypress', function(event) {
+    if (event.key === 'm');{
+        window.location.replace(href="..templates\Error_404_Display.html");
+    }
+  }));
