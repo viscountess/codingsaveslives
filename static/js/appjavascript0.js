dca09ec1f976a511623cc99c04a1582927bdf99e
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2020-04-25 13:56:50 +0100

    javascript changes
    made 'welcome message' just appear on home page, not every page. will need to ensure landing page is therefore linked to that javascript.

--- static/js/appjavascript0.js
+++ static/js/appjavascript0.js
@@ -1,7 +1,3 @@
-//explaining the m Error page shortcut;
-var welcome = "Welcome to Maia! If you ever need to hide your screen, just click the button on the side, or type 'm' and we will show an Error page! For your safety, it will not be possible to see what you were previously looking at by then pressing the 'back' button.";
-    (alert(welcome));
-
 
 //m trigger;
 (document.addEventListener('keypress', function(event) {
