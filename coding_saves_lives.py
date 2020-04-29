9491fa29840bbb4680f1b680f93ebca84c866631
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2020-04-29 17:10:01 +0100

    fixed python/flask setup

--- coding_saves_lives.py
+++ coding_saves_lives.py
@@ -2,30 +2,26 @@ from flask import Flask, render_template, request, Response #importds functions 
 
 app = Flask("saves_lives_app") #making an app
 
+#Homepage
 @app.route("/")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
 def landing_page():
         return render_template("landing_page.html") #runs the landing page
 
-#Forum pages
-@app.route("/forum")  
-def forum_landing_page():
-        return render_template("forum_landing_page.html") #runs the forum landing page
-
-@app.route("/ask_a_question")
-def forum_new_question_page():
-        return render_template("forum_new_question_page.html") #runs the new question submission page
-
 #Resources
 @app.route("/resources")
 def resources_landing_page():
-        return render_template("resources_landing_page.html") #runs the resources page
+        return render_template("resources_quiz_page.html") #runs the resources page
+
+@app.route("/resources_list")
+def resources_landing_page():
+        return render_template("resources_list_page.html") #runs the resources page
 
 #Decoy
 @app.route("/error_404_display")
 def Error_404_Display():
         return render_template("Error_404_Display.html") #runs the fact that this url shows the display an error page
 
-#about Maia pages
+#other pages
 @app.route("/about")
 def about_us():
         return render_template("about_us.html") #runs the fact that this url shows the about page
@@ -34,9 +30,9 @@ def about_us():
 def privacy_policy():
         return render_template("privacy_policy.html") #runs the fact that this url shows the PP page
 
-@app.route("/code_of_conduct")
-def Code_of_Conduct():
-        return render_template("Code_of_Conduct.html") #runs the fact that this url shows the code of conduct page
+@app.route("/identify_it")
+def privacy_policy():
+        return render_template("what_is_domestic_abuse.html") #runs the fact that this url shows the PP page
 
 #debugging
 app.run(debug=True) #runs the app. the debug part - unlocks debugging feature.
