007ac9ec1c7e0f1691f6c12da2c52625b24baab8
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2020-05-01 20:10:53 +0100

    fixed js; found problem with form

--- static/js/appjavascript0.js
+++ static/js/appjavascript0.js
@@ -1,8 +1,12 @@
 
+window.history.forward(); 
+function noBack() { 
+    window.history.forward();}
+    
 function KeyPress(e) {
     var evtobj = window.event? event : e
     if (evtobj.keyCode == 77 && evtobj.ctrlKey){
-        window.location.replace(href="Error_404_Display.html");
+        window.location.replace(href="/error_404_display");
     }
 }
 document.onkeydown = KeyPress;
